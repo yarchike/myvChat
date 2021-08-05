89c4f9a825a54d9661baee336e5a1fdedde202a2
Yarchike <yarchike@gmail.com>
2021-08-05 08:54:29 +0300

    25 work

--- app/src/main/java/com/martynov/myvchat/MainActivity.kt
+++ app/src/main/java/com/martynov/myvchat/MainActivity.kt
@@ -50,7 +50,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        AppStates.updateState(AppStates.ONLINE)
+    }
 
-
-
+    override fun onStop() {
+        super.onStop()
+        AppStates.updateState(AppStates.OFFLINE)
+    }
 }
