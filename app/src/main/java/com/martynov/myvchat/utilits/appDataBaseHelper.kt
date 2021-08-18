2a80d757c6d9ffa4a15b8997a1a02a6378538b3e
Yarchike <yarchike@gmail.com>
2021-08-18 15:16:31 +0300

    31 work

--- app/src/main/java/com/martynov/myvchat/utilits/appDataBaseHelper.kt
+++ app/src/main/java/com/martynov/myvchat/utilits/appDataBaseHelper.kt
@@ -19,6 +19,8 @@ lateinit var USER: User
 
 const val NODE_USERS = "users"
 const val NODE_USERNAMES = "usernames"
+const val NODE_PHONES = "phones"
+const val NODE_PHONES_CONTACTS = "phones_contacts"
 
 const val FOLDER_PROFILE_IMAGE = "profile_image"
 
@@ -101,5 +103,20 @@ fun initContacts() {
             }
         }
         cursor?.close()
+        updatePhonesToDataBase(arrayContacts)
     }
+}
+
+fun updatePhonesToDataBase(arrayContacts: ArrayList<CommoModel>) {
+    REF_DATABASE_ROOT.child(NODE_PHONES).addListenerForSingleValueEvent(AppValueEventListener{
+        it.children.forEach { snapshot ->
+            arrayContacts.forEach { contact ->
+                if(snapshot.key == contact.phone){
+                    REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)
+                        .child(snapshot.value.toString()).child(CHILD_ID).setValue(snapshot.value.toString())
+                        .addOnFailureListener{ showToast(it.message.toString())}
+                }
+            }
+        }
+    })
 }
